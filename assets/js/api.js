d73ecf9ab9b232ac11965af18f523341f48441c6
HugoCNRD <hugobatigne@gmail.com>
2026-07-30 23:45:21 +0200

    Implement API function for POST requests
    Added API_URL constant and implemented api function for making POST requests.

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,1 +1,41 @@
+const API_URL =
+"https://crew-center-api.hugobatigne.workers.dev/";
 
+
+
+
+
+async function api(action,data={}){
+
+
+    const response =
+    await fetch(API_URL,{
+
+        method:"POST",
+
+        headers:{
+
+            "Content-Type":
+            "application/json"
+
+        },
+
+
+        body:JSON.stringify({
+
+            action:action,
+
+            ...data
+
+        })
+
+
+    });
+
+
+
+    return await response.json();
+
+
+
+}
