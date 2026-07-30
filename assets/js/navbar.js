b795fced77df359c6fc59c7226b34366ce9e841c
HugoCNRD <hugobatigne@gmail.com>
2026-07-30 23:46:23 +0200

    Add user session handling in navbar

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,1 +1,84 @@
+document.addEventListener(
+"DOMContentLoaded",
+()=>{
 
+
+const user =
+getSession();
+
+
+
+if(!user) return;
+
+
+
+const username =
+document.getElementById(
+"userName"
+);
+
+
+
+if(username){
+
+username.innerHTML =
+user.username;
+
+}
+
+
+
+const rank =
+document.getElementById(
+"userRank"
+);
+
+
+
+if(rank){
+
+rank.innerHTML =
+user.rank;
+
+}
+
+
+
+
+const staffButton =
+document.getElementById(
+"staffAccess"
+);
+
+
+
+if(
+staffButton
+&&
+!isStaff()
+){
+
+staffButton.style.display="none";
+
+}
+
+
+
+
+const logoutButton =
+document.getElementById(
+"logoutButton"
+);
+
+
+
+if(logoutButton){
+
+logoutButton.onclick =
+logout;
+
+}
+
+
+
+});
